ad24ba9cbeb478af648e8b08e313c3ce83b266ef
jugo92 <jugzek92@outlook.fr>
2023-10-11 17:05:09 +0200

    push test todoList

--- src/todoList/todoList.test.ts
+++ src/todoList/todoList.test.ts
@@ -31,3 +31,33 @@ test('Max items', () => {
 
     expect(() => todoList.addItem(item)).toThrow("Email could not be sent.");
   });
+
+  test('Bad Item', () => {
+    const user = new User("john@gmail.com", "John", "Doe", new Date(new Date().setFullYear(new Date().getFullYear() - 15)), '')
+
+    const date = new Date();
+
+    date.setHours(0, 0, 0, 0);
+    const items = Array.from({ length: 7 }, () => new Item("Stylo", "Stylo Vert", date, 1));
+    const todoList = new ToDoList(user, items, "Todo", date); 
+
+    const item = new Item("Stylo", "", new Date(), 1);
+
+
+    expect(() => todoList.addItem(item)).toThrow("Item invalide.");
+  });
+
+  test('Time violated insert', () => {
+    const user = new User("john@gmail.com", "John", "Doe", new Date(new Date().setFullYear(new Date().getFullYear() - 15)), '')
+
+    const date = new Date();
+
+    date.setHours(0, 0, 0, 0);
+    const items = Array.from({ length: 7 }, () => new Item("Stylo", "Stylo Vert", date, 1));
+    const todoList = new ToDoList(user, items, "Todo", new Date()); 
+
+    const item = new Item("Stylo", "Stylo Bleu", new Date(), 1);
+
+
+    expect(() => todoList.addItem(item)).toThrow("Time constraint violated.");
+  });
