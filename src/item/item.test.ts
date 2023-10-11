3494dadbf03de06134259ee3d2be028a6b710d7b
dan1M <maneadaniel555@gmail.com>
2023-10-11 17:17:28 +0200

    fix item test

--- src/item/item.test.ts
+++ src/item/item.test.ts
@@ -1,12 +1,21 @@
-import Item from "./item";
+import Item from './item';
 
 test('Empty name', () => {
-    const item = new Item("", "Contenu du produit", new Date(), 1);
-    expect(item.isValid()).toBe(false);
-  });
+  const item = new Item('', 'Contenu du produit', new Date(), 1);
+  expect(item.isValid()).toBe(false);
+});
 
-  test('Empty content', () => {
-    const item = new Item("Stylo", "", new Date(), 1);
-    expect(item.isValid()).toBe(false);
-  });
-  
+test('Empty content', () => {
+  const item = new Item('Stylo', '', new Date(), 1);
+  expect(item.isValid()).toBe(false);
+});
+
+test('Content ok', () => {
+  const item = new Item(
+    'Stylo',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sit amet aliquet ultricies, nisl massa consectetur magna, a aliquam nisl nunc vitae sem. Sed euismod, nisl sit amet aliquet ultricies, nisl massa consectetur magna, a aliquam nisl nunc vitae sem.',
+    new Date(),
+    1
+  );
+  expect(item.isValid()).toBe(true);
+});
