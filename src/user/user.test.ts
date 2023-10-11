22c72ec501d929dcb37efef9e581205d860c58fc
dan1M <maneadaniel555@gmail.com>
2023-10-11 15:59:01 +0200

    added todolist, password to user

--- src/user/user.test.ts
+++ src/user/user.test.ts
@@ -1,11 +1,35 @@
 import User from './user';
 
 test('user is valid', () => {
-  const user = new User('test@test', 'test', 'test', '2010-01-01');
+  const user = new User(
+    'test@test.com',
+    'test',
+    'test',
+    new Date(new Date().setFullYear(new Date().getFullYear() - 15)),
+    'testSAZE1234'
+  );
   expect(user.isValid()).toBe(true);
 });
 
 test('user is invalid without email', () => {
-  const user = new User('test@test.com', '', 'test', '2002-01-01');
+  const user = new User(
+    '',
+    'test',
+    'test',
+    new Date(new Date().setFullYear(new Date().getFullYear() - 15)),
+    'testSAZE1234'
+  );
+
+  expect(user.isValid()).toBe(false);
+});
+
+test('user is invalid without firstName', () => {
+  const user = new User(
+    'test@test.com',
+    '',
+    'test',
+    new Date(new Date().setFullYear(new Date().getFullYear() - 15)),
+    'testSAZE1234'
+  );
   expect(user.isValid()).toBe(false);
 });
--- src/user/user.ts
+++ src/user/user.ts
@@ -1,32 +1,58 @@
 import isEmail from 'validator/lib/isEmail';
+import isEmpty from 'validator/lib/isEmpty';
+import ToDoList from '../todoList/todoList';
 
 class User {
   email: string;
   firstName: string;
   lastName: string;
   birthDate: Date;
+  password: string;
+  todolist: ToDoList;
 
   constructor(
     email: string,
     firstName: string,
     lastName: string,
-    birthDate: Date
+    birthDate: Date,
+    password: string
   ) {
     this.email = email;
+    this.password = password;
     this.firstName = firstName;
     this.lastName = lastName;
     this.birthDate = birthDate;
   }
 
-  getEmail(): string {
-    return this.email;
+  createTodolist() {
+    if (this.isValid()) {
+      this.todolist = new ToDoList(this, [], new Date());
+    }
   }
 
   isValid() {
-    if (isEmail(this.email)) {
+    if (!isEmail(this.email)) {
       return false;
     }
     console.log('good email');
+
+    // Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number.
+    if (
+      isEmpty(this.password) ||
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,40}$/.test(this.password) === false
+    ) {
+      return false;
+    }
+
+    if (isEmpty(this.firstName)) {
+      return false;
+    }
+    console.log('good first name');
+
+    if (isEmpty(this.lastName)) {
+      return false;
+    }
+    console.log('good last name');
 
     const today = new Date();
     const age = today.getFullYear() - this.birthDate.getFullYear();
