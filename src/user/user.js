47dd29024ec22e8e414a617150c9f2e1ee7d2c41
Numericite Dev <numericitedev@Numericites-MacBook-Air.local>
2023-10-11 11:12:15 +0200

    init

---
+++ src/user/user.js
@@ -0,0 +1,35 @@
+class User {
+  constructor(email, firstName, lastName, birthDate) {
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthDate = birthDate;
+  }
+
+  isValid() {
+    if (!this.email || !this.email.includes('@')) {
+      return false;
+    }
+    console.log('good email');
+    if (!this.firstName || !this.lastName) {
+      return false;
+    }
+    console.log('good name');
+
+    if (!this.birthDate) {
+      return false;
+    }
+    console.log('good birthdate');
+    const today = new Date();
+    const birthDate = new Date(this.birthDate);
+    const age = today.getFullYear() - birthDate.getFullYear();
+    if (age < 13) {
+      return false;
+    }
+    console.log('good age');
+
+    return true;
+  }
+}
+
+module.exports = User;
---
+++ src/user/user.test.js
@@ -0,0 +1,11 @@
+const User = require('./user');
+
+test('user is valid', () => {
+  const user = new User('test@test', 'test', 'test', '2010-01-01');
+  expect(user.isValid()).toBe(true);
+});
+
+test('user is invalid without email', () => {
+  const user = new User(null, 'test', 'test', '2002-01-01');
+  expect(user.isValid()).toBe(true);
+});
